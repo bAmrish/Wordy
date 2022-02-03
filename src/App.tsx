fbfc4783fe183b70a5b791ea72bce337cc202542
Amrish Bharatiya <bamrish@gmail.com>
2022-02-03 19:40:51 +0530

    Added some sort of "theme".

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import classes from './App.module.css';
-import Game from "./game/Game";
+import Game from './game/Game';
+import Header from './header/Header';
 
 function App() {
   return (
     <div className={classes.app}>
-      <header className={classes["app-header"]}><h1>Wordy</h1></header>
-      <Game />
+      <Header />
+      <div className={classes['main-container']}>
+        <Game />
+      </div>
     </div>
   );
 }
