7818484086cc9cbe947d45ca70ff2405b6f2120a
Amrish Bharatiya <bamrish@gmail.com>
2022-02-13 21:02:22 +0530

    Added code to calculate current and longest streak

--- src/stats/play-statistics/PlayStatistics.tsx
+++ src/stats/play-statistics/PlayStatistics.tsx
@@ -2,6 +2,45 @@ import classes from './PlayStatistics.module.css';
 import GameModel from '../../game/models/game.model';
 import { FC } from 'react';
 
+const getLongestStreak = (allGames: GameModel[]): number => {
+  const games = allGames.slice();
+  let longestStreak = games.findIndex(game => game.status === 'LOST');
+
+  if (longestStreak === -1) {
+    return games.length;
+  }
+
+  while (games.length > 0) {
+    const currentStreak = games.findIndex(game => game.status === 'LOST');
+    if (currentStreak > longestStreak) {
+      longestStreak = currentStreak;
+    }
+
+    if (currentStreak === -1) {
+      if (games.length > longestStreak) {
+        longestStreak = games.length;
+      }
+      break;
+    }
+    games.splice(0, currentStreak + 1);
+  }
+
+  return longestStreak;
+};
+
+const getCurrentStreak = (games: GameModel[]): number => {
+  // Games are by default sorted by ascending order of created date.
+  // If this assumption changes, change the reverse call to `sort` call.
+  const currentStreak = games
+    .slice()
+    .reverse()
+    .findIndex(game => game.status === 'LOST');
+  if (currentStreak === -1) {
+    return games.length;
+  }
+  return currentStreak;
+};
+
 const PlayStatistics: FC<{ games: GameModel[] }> = props => {
   const games = props.games.filter(game => game.status !== 'PLAYING');
 
@@ -10,6 +49,9 @@ const PlayStatistics: FC<{ games: GameModel[] }> = props => {
   const lost = games.filter(game => game.status === 'LOST').length;
   const percentWon = played > 0 ? (won * 100) / played : 0;
 
+  const longestStreak = getLongestStreak(games.slice());
+  const currentStreak = getCurrentStreak(games.slice());
+
   return (
     <div className={classes['play-statistics']}>
       <div className={classes['cards']}>
@@ -17,6 +59,10 @@ const PlayStatistics: FC<{ games: GameModel[] }> = props => {
         <Card value={won} title="Won" className={classes.won} />
         <Card value={lost} title="Lost" className={classes.lost} />
         <Card value={percentWon.toFixed(2) + ' %'} title="% Won" />
+      </div>
+      <div className={classes['cards']}>
+        <Card value={currentStreak} title="Current Streak" />
+        <Card value={longestStreak} title="Longest Streak" />
       </div>
     </div>
   );
